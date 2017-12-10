ece868d506db39445dcc8d18b6674602a494450e
PhD20 <dmprofessor227@gmail.com>
2017-12-09 23:04:59 -0600

    First Commit
    Time to get rolling!

---
+++ Manticore/Manticore/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Manticore.Models;
+
+namespace Manticore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Setup()
+        {
+            ViewData["Message"] = "Project Manticore";
+
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            ViewData["Message"] = "Project Manticore is not live yet.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
